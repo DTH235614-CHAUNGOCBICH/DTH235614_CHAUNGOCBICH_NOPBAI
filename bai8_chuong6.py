e0cde90220e829a6d360b897a17e0a05b7b27485
DTH235614-CHAUNGOCBICH <bich_dth235614@student.agu.edu.vn>
2025-10-07 15:03:26 +0700

    Add files via upload

---
+++ bai8_chuong6.py
@@ -0,0 +1,9 @@
+n = int(input("Nhập số phần tử: "))
+M = []
+
+for i in range(n):
+    M.append(float(input(f"Nhập M[{i}]: ")))
+
+print("Dãy số ban đầu:", M)
+M.sort(reverse=True)
+print("Dãy số sau khi sắp xếp giảm dần:", M)
